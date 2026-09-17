fdc76647775e860cb9a6cc0ba5307062e2d42633
Bakir-dev09 <bibobbobib467@gmail.com>
2026-09-17 16:43:46 +0600

    added group models and schema also added age for room

--- migrations/env.py
+++ migrations/env.py
@@ -1,9 +1,10 @@
 from logging.config import fileConfig
-from mysite.db.models import *
+
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-from alembic import context
 
+from alembic import context
+from mysite.db.models import *
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
--- migrations/versions/d6970c13df62_.py
+++ migrations/versions/d6970c13df62_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 094318648faa
+Revision ID: d6970c13df62
 Revises: 
-Create Date: 2026-09-10 13:31:55.162117
+Create Date: 2026-09-17 15:49:06.846255
 
 """
 from typing import Sequence, Union
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = '094318648faa'
+revision: str = 'd6970c13df62'
 down_revision: Union[str, Sequence[str], None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
@@ -40,6 +40,13 @@ def upgrade() -> None:
     sa.PrimaryKeyConstraint('id'),
     sa.UniqueConstraint('email'),
     sa.UniqueConstraint('username')
+    )
+    op.create_table('refresh_token',
+    sa.Column('id', sa.Integer(), autoincrement=True, nullable=False),
+    sa.Column('token', sa.String(), nullable=False),
+    sa.Column('user_id', sa.Integer(), nullable=False),
+    sa.ForeignKeyConstraint(['user_id'], ['user_profile.id'], ),
+    sa.PrimaryKeyConstraint('id')
     )
     op.create_table('room',
     sa.Column('id', sa.Integer(), autoincrement=True, nullable=False),
@@ -176,6 +183,7 @@ def downgrade() -> None:
     op.drop_table('user_statistics')
     op.drop_table('user_achievement')
     op.drop_table('room')
+    op.drop_table('refresh_token')
     op.drop_table('user_profile')
     op.drop_table('achievement')
     # ### end Alembic commands ###
